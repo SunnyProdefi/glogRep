0664d66faeca952e7235abc7fc304752797cb2ae
sunnyprodefi <sunprodefi@gmail.com>
2024-04-30 17:48:04 +0800

    Revise glogExample.cc

--- glogExample.cc
+++ glogExample.cc
@@ -1,18 +1,16 @@
 #include <glog/logging.h>
 
 int main(int argc, char* argv[]) {
-    // 初始化Glog库
+    FLAGS_logtostderr = true;  // 强制所有日志输出到 stderr
+    // 初始化 glog
     google::InitGoogleLogging(argv[0]);
 
-    // 设置日志文件的存储路径和前缀
-    FLAGS_log_dir = "/var/tmp";
+    // 使用不同的日志级别
     LOG(INFO) << "This is an info message";
     LOG(WARNING) << "This is a warning message";
     LOG(ERROR) << "This is an error message";
-
-    // 检查条件，如果不满足则记录FATAL级别日志并终止程序
-    int x = 9;
-    CHECK(x > 10) << "x is less than 10!";
-
+    for (int i = 0; i < 10; ++i) {
+        LOG_FIRST_N(INFO, 3) << "Log this message only the first 3 times";
+    }
     return 0;
 }
